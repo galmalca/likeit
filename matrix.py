e5c3e6b4768177080065c036756557b56044aac9
gal <galmalca10>
2017-05-29 13:57:07 +0300

    mongo

--- matrix.py
+++ matrix.py
@@ -6,12 +6,11 @@ TIMER = 5 * 60
 from MF import MatrixFactorization as mf
 
 moranMongo = MongoClient('mongodb://galevgi:galgalgal@ds133981.mlab.com:33981/likeitarticle')
-localMongo = MongoClient('mongodb://project57:likeit1234@127.0.0.1:27017')
-db = localMongo.DB57
+localMongo = MongoClient('mongodb://project57:likeit1234@127.0.0.1:27017/DB57')
 
 
 def schedule():
-    user = localMongo.db.users
+    user = localMongo.DB57.users
     users = list(user.find())
     mf.MatrixFactorization.loadFileToData(users)
     mf.MatrixFactorization.makeMatrix()
